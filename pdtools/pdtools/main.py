68b799b773383a4392a3fabc572cadcd299004d9
damouse <mickey.barboi@gmail.com>
2015-07-07 20:38:33 -0500

    chute loading stable

--- pdtools/pdtools/main.py
+++ pdtools/pdtools/main.py
@@ -1,8 +1,8 @@
 """Paradrop build tools.
 
 Usage:
-    paradrop install <chute-directory> <host> <port>
-    paradrop snap-install <host> <port>`
+    paradrop install <host> <port> <github-url> 
+    paradrop snap-install <host> <port>
     paradrop (-h | --help)
     paradrop --version
 Options:
@@ -14,6 +14,7 @@ Options:
 from docopt import docopt
 import requests
 import os
+import json
 
 
 def main():
@@ -24,13 +25,13 @@ def main():
     # print(args)
 
     if args['install']:
-        installChute(args['<host>'], args['<port>'], args['<chute-directory>'])
+        installChute(args['<host>'], args['<port>'], args['<github-url>'])
 
     if args['snap-install']:
         print 'Not implemented. Sorry, love.'
 
 
-def installChute(host, port, directory):
+def installChute(host, port, github):
     '''
     Testing method. Take a git url from github and load it onto snappy-pd. 
     '''
@@ -42,11 +43,9 @@ def installChute(host, port, directory):
     # The project directory can either be cloned to the backend or installed locally,
     # doesn't matter which one-- we still have to stream the files over
 
-    params = {'url': directory}
-    r = requests.post('http://' + host + ':' + str(port) + '/v1/chute/create', params=params)
+    params = {'url': github}
+    r = requests.post('http://' + host + ':' + str(port) + '/v1/chute/create', data=json.dumps(params))
     print r.text
-
-    print 'Finished installing chute'
 
 
 def installParadrop():
