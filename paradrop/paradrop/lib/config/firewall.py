6c665f2082f1e29725363eaa0f953910bdadd63c
Lance Hartung <hartung@cs.wisc.edu>
2015-07-11 17:07:41 -0500

    Implemented chute redirect firewall rules.

--- paradrop/paradrop/lib/config/firewall.py
+++ paradrop/paradrop/lib/config/firewall.py
@@ -1,6 +1,19 @@
+import fnmatch
+
 from paradrop.lib.config import configservice, uciutils
 from paradrop.lib.utils import uci
 from paradrop.lib.utils.output import out, logPrefix
+
+def findMatchingInterface(iface_name, interfaces):
+    """
+    Search an interface list for one matching a given name.
+
+    iface_name can contain shell-style wildcards (* and ?).
+    """
+    for iface in interfaces:
+        if fnmatch.fnmatch(iface['name'], iface_name):
+            return iface
+    return None
 
 def getOSFirewallRules(update):
     """
@@ -36,6 +49,48 @@ def getOSFirewallRules(update):
         # Then add a forwarding rule for wan type interfaces.
         if iface['netType'] == 'wan':
             rules.append(({'type': 'forwarding'}, {'src': iface['externalIntf'], 'dest': 'wan'}))
+
+    if hasattr(update.new, "firewall"):
+        for rule in update.new.firewall:
+            if rule['type'] == 'redirect':
+                config = {'type': 'redirect'}
+                options = {
+                    'name': rule['name'],
+                    'proto': 'tcpudp'
+                }
+
+                from_parts = rule['from'].strip().split(':')
+                to_parts = rule['to'].strip().split(':')
+
+                # From @host.lan means this is a DNAT rule (redirect to the chute).
+                if from_parts[0] == "@host.lan":
+                    options['target'] = "DNAT"
+                    
+                    options['src'] = "wan"
+                    if len(from_parts) > 1:
+                        options['src_dport'] = from_parts[1]
+
+                    # Find the interface specified in the rule, so we can get its
+                    # IP address.
+                    iface = findMatchingInterface(to_parts[0], interfaces)
+                    if iface is None:
+                        out.warn("** {} No interface found with name {}".format(
+                            logPrefix(), to_parts[0]))
+                        raise Exception("Interface not found")
+
+                    options['dest_ip'] = iface['externalIpaddr']
+                    if len(to_parts) > 1:
+                        options['dest_port'] = to_parts[1]
+
+                # This is an SNAT rule (redirect from the chute to host network).
+                else:
+                    options['target'] = "SNAT"
+
+                    # TODO: Implement
+                    out.warn("** {} SNAT rules not supported yet".format(logPrefix()))
+                    raise Exception("SNAT rules not implemented")
+
+                ruless.append((config, options))
 
     update.new.setCache('osFirewallRules', rules)
 
