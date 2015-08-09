a11b60f110fc37bfaa928b1c13645b5c97dc3f9c
damouse <mickey.barboi@gmail.com>
2015-08-09 14:12:13 -0500

    new docs on tools

--- pdtools/pdtools/main.py
+++ pdtools/pdtools/main.py
@@ -18,17 +18,6 @@ usage:
 options:
     -h --help     Show this screen.
     --version     Show version.
-
-The most commonly used paradrop commands are:
-   add        Add file contents to the index
-   branch     List, create, or delete branches
-   checkout   Checkout a branch or paths to the working tree
-   clone      Clone a repository into a new directory
-   commit     Record changes to the repository
-   push       Update remote refs along with associated objects
-   remote     Manage set of tracked repositories
-
-See 'paradrop help <command>' for more information on a specific command.
 '''
 
 from pkg_resources import get_distribution
@@ -47,23 +36,12 @@ SERVER_PORT = 8015
 
 
 def main():
-    # For now, don't grab STDIO and don't write random log noise to conosle
-    output.out.startLogging(stealStdio=False, printToConsole=False)
-
     # show the documentation and extract host and port if provided (since they are commonly used)
     args = docopt(__doc__, version=get_distribution('pdtools').version)
     host, port = args['<host>'], args['<port>']
     port = int(port) if port else None
 
-    # Initialize riffle with default values (can be overridden later)
-    riffle.portal.certCa = store.getKey('ca.pem')
-    riffle.portal.keyPrivate = store.getKey('client.pem')
-    riffle.portal.host = SERVER_HOST
-    # NOTE: riffle serves on its own default port. This is a different port from the const above
-
-    # Register realms. See riffle documentation for Realm in pdtools.lib.riffle
-    riffle.portal.addRealm(names.matchers[names.NameTypes.server], riffle.Realm(server.ServerPerspective))
-    riffle.portal.addRealm(names.matchers[names.NameTypes.router], riffle.Realm(riffle.RifflePerspective))
+    setup()
 
     # Make it happen.
     if args['install']:
@@ -110,6 +88,85 @@ def main():
         task.react(routers.provisionRouter, (args['<name>'], args['<host>'], args['<port>']))
 
     print 'Something broke'
+
+
+def setup():
+    '''
+    Boilerplate setup. Start the logger, give riffle crypto keys, and 
+    initialize riffle's portal by creating name to realm assignments
+    '''
+    # For now, don't grab STDIO and don't write random log noise to conosle
+    output.out.startLogging(stealStdio=False, printToConsole=False)
+
+    # Initialize riffle with default values (can be overridden later)
+    riffle.portal.certCa = store.getKey('ca.pem')
+    riffle.portal.keyPrivate = store.getKey('client.pem')
+    riffle.portal.host = SERVER_HOST
+    # NOTE: riffle serves on its own default port. This is a different port from the const above
+
+    # Register realms. See riffle documentation for Realm in pdtools.lib.riffle
+    riffle.portal.addRealm(names.matchers[names.NameTypes.server], riffle.Realm(server.ServerPerspective))
+    riffle.portal.addRealm(names.matchers[names.NameTypes.router], riffle.Realm(riffle.RifflePerspective))
+
+
+###################################################################
+# New Docs (currently incomplete)
+###################################################################
+
+rootDoc = """
+usage: git [--version] [--exec-path=<path>] [--html-path]
+           [-p|--paginate|--no-pager] [--no-replace-objects]
+           [--bare] [--git-dir=<path>] [--work-tree=<path>]
+           [-c <name>=<value>] [--help]
+           <command> [<args>...]
+
+options:
+   -c <name=value>
+   -h, --help
+   -p, --paginate
+
+The most commonly used git commands are:
+   add        Add file contents to the index
+   branch     List, create, or delete branches
+   checkout   Checkout a branch or paths to the working tree
+   clone      Clone a repository into a new directory
+   commit     Record changes to the repository
+   push       Update remote refs along with associated objects
+   remote     Manage set of tracked repositories
+
+See 'git help <command>' for more information on a specific command.
+
+"""
+routerDoc = """
+usage: paradrop router provision
+       paradrop router list
+
+    -v, --verbose         be verbose; must be placed before a subcommand
+
+Other Content!
+    Look!
+"""
+
+
+def routerMenu(argv):
+    args = docopt(routerDoc)
+    print args
+
+
+def main2():
+    ''' Show documentation and branch appropriately '''
+    # show the documentation and extract host and port if provided (since they are commonly used)
+    args = docopt(rootDoc, version=get_distribution('pdtools').version)
+
+    # host, port = args['<host>'], args['<port>']
+    # port = int(port) if port else None
+    argv = [args['<command>']] + args['<args>']
+
+    if args['<command>'] == 'remote':
+        # routerMenu(argv)
+        args = docopt(routerDoc)
+        print args
+
 
 if __name__ == '__main__':
     main()
