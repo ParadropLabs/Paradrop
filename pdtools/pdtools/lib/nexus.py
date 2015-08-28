be406762ef8c848f731e2091c10a27266d1bba48
damouse <mickey.barboi@gmail.com>
2015-08-28 18:16:52 -0500

    all kinds of nexus goodness

--- pdtools/pdtools/lib/nexus.py
+++ pdtools/pdtools/lib/nexus.py
@@ -165,16 +165,12 @@ class NexusBase(object):
         [x._lock() for x in [self.paths, self.net, self.meta]]
         self.info.setOnChange(self.onInfoChange)
 
+        # Done with data initizlization. From here on out its configuration and state
+
         # initialize output. If filepath is set, logs to file.
         # If stealStdio is set intercepts all stderr and stdout and interprets it internally
         # If printToConsole is set (defaults True) all final output is rendered to stdout
         # output.out.startLogging(filePath=self.logPath, stealStdio=stealStdio, printToConsole=printToConsole)
-
-        # Asssign global riffle keys
-        # riffle.portal.keyPrivate = self.getKey('pub')
-        # riffle.portal.certCa = self.getKey('ca')
-        # riffle.portal.host = self.serverHost()
-        # riffle.portal.port = self.rifflePort()
 
         # register onStop for the shutdown call
         # reactor.addSystemEventTrigger('before', 'shutdown', self.onStop)
