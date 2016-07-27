3d0cfb164a9fd5191bb62239fe159e1ddfa0fdd1
Lance Hartung <lance@paradrop.io>
2016-07-27 10:46:10 -0500

    Mocked out reactor.listenTCP to fix failing tests.

--- tests/paradrop/test_main.py
+++ tests/paradrop/test_main.py
@@ -3,27 +3,48 @@ from mock import Mock, patch
 
 import sys
 
+
+@patch("twisted.internet.reactor.listenTCP")
 @patch("paradrop.backend.pdconfd.main.run_pdconfd")
 @patch("paradrop.backend.pdconfd.main.run_thread")
 @patch("paradrop.backend.pdfcd.server.setup")
-def test_paradrop_main(setup, run_thread, run_pdconfd):
+def test_paradrop_main(setup, run_thread, run_pdconfd, listenTCP):
     """
     Test paradrop main function
     """
     from paradrop.main import main
 
-    args = {
-        'mode': 'production'
-    }
-
     sys.argv = ["pd"]
     main()
     assert run_thread.called
     assert setup.called
+    assert listenTCP.called
 
-    sys.argv.append("--local")
+
+@patch("twisted.internet.reactor.listenTCP")
+@patch("paradrop.backend.pdconfd.main.run_pdconfd")
+@patch("paradrop.backend.pdconfd.main.run_thread")
+@patch("paradrop.backend.pdfcd.server.setup")
+def test_paradrop_main_local(setup, run_thread, run_pdconfd, listenTCP):
+    """
+    Test paradrop main function (--local)
+    """
+    from paradrop.main import main
+
+    sys.argv = ["pd", "--local"]
     main()
+    assert listenTCP.called
 
-    sys.argv.append("--config")
+
+@patch("paradrop.backend.pdconfd.main.run_pdconfd")
+@patch("paradrop.backend.pdconfd.main.run_thread")
+@patch("paradrop.backend.pdfcd.server.setup")
+def test_paradrop_main_config(setup, run_thread, run_pdconfd):
+    """
+    Test paradrop main function (--config)
+    """
+    from paradrop.main import main
+
+    sys.argv = ["pd", "--config"]
     main()
     assert run_pdconfd.called
