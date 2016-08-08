2e18a259a9e168c919efbab541e3c1213dd8a2a9
Peng Liu <pengliu.mail@gmail.com>
2016-08-08 17:06:07 -0500

    Updated the local portal to include status page

--- paradrop/paradrop/backend/pdfcd/apistatus.py
+++ paradrop/paradrop/backend/pdfcd/apistatus.py
@@ -6,7 +6,7 @@ from pdtools.lib.output import out
 from paradrop.lib.api.pdrest import APIDecorator
 from paradrop.lib.api import pdapi
 
-class StatusAPI(object):
+class StatusAPI:
     """
     Status API.
 
@@ -23,9 +23,9 @@ class StatusAPI(object):
         A method for getting system information
         """
         data = dict()
-        data['cpu'] = psutil.cpu_percent(interval=0.2)
+        data['cpu'] = psutil.cpu_percent()
         data['memory'] = psutil.virtual_memory()
-        data['disks'] = psutil.disk_usage('/')
+        data['disks'] = psutil.disk_usage('/writable')
         data['net'] = psutil.net_io_counters(True)
         result = json.dumps(data)
         apiPkg.request.setHeader('Content-Type', 'application/json')
@@ -34,7 +34,8 @@ class StatusAPI(object):
     @APIDecorator()
     def GET_netinfo(self, apiPkg):
         data = dict()
-        data['interface'] = psutil.net_if_addrs()
+        data['addresses'] = psutil.net_if_addrs()
+        data['stats'] = psutil.net_if_stats()
         result = json.dumps(data)
         apiPkg.request.setHeader('Content-Type', 'application/json')
         apiPkg.setSuccess(result)
