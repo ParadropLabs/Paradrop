9bd5007ef416d6af1a11af6e71fadc9ad5cf1a27
Peng Liu <pengliu.mail@gmail.com>
2016-08-18 13:16:43 -0500

    Get back Lance's update about hostapd snap.

--- snappy_v1/build_image.sh
+++ snappy_v1/build_image.sh
@@ -5,6 +5,6 @@ sudo ubuntu-device-flash core 15.04 --channel stable \
     --install paradrop_0.2.0_all.snap \
     --install pdinstall_0.2.0_all.snap \
     --install dnsmasq_2.74_all.snap \
-    --install hostapd_2.4_all.snap \
+    --install hostapd_2.4-1_all.snap \
     --developer-mode \
     --output paradrop_router.img
--- snappy_v1/download_snaps.sh
+++ snappy_v1/download_snaps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DNSMASQ_SNAP="https://paradrop.io/storage/snaps/dnsmasq_2.74_all.snap"
-HOSTAPD_SNAP="https://paradrop.io/storage/snaps/hostapd_2.4_all.snap"
+HOSTAPD_SNAP="https://paradrop.io/storage/snaps/hostapd_2.4-1_all.snap"
 
 if [ ! -f dnsmasq_2.74_all.snap ]; then
   wget --quiet $DNSMASQ_SNAP
