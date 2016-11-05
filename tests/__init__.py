f3ace720f38c2ea6219d266a927d805aa40ba2f9
Peng Liu <pengliu.mail@gmail.com>
2016-11-04 22:43:20 -0500

    Make sure most unit test cases pass

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,4 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('pdinstall'))
-sys.path.insert(0, os.path.abspath('pdtools'))
-sys.path.insert(0, os.path.abspath('paradrop'))
+sys.path.insert(0, os.path.abspath('paradrop/src'))
