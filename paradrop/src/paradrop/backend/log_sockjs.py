f64172fa5598274e92feabf208396fb2b620e2ea
Lance Hartung <hartung@cs.wisc.edu>
2017-01-30 10:30:39 -0600

    Fixes to make chute log provider functional.

--- paradrop/src/paradrop/backend/log_sockjs.py
+++ paradrop/src/paradrop/backend/log_sockjs.py
@@ -2,12 +2,16 @@ import json
 from twisted.internet.protocol import Protocol, Factory
 
 from paradrop.base.output import out
+from paradrop.core.container.log_provider import LogProvider
 
 
 class LogSockJSProtocol(Protocol):
-    def __init__(self, factory):
+    def __init__(self, factory, log_provider):
         self.factory = factory
-        self.queue = factory.log_provider.attach()
+        self.log_provider = log_provider
+
+        self.queue = None
+        self.queueDeferred = None
 
     def connectionMade(self):
         if not hasattr(self.factory, "transports"):
@@ -15,21 +19,36 @@ class LogSockJSProtocol(Protocol):
         self.factory.transports.add(self.transport)
         out.info('sockjs /logs connected')
 
-        d = self.queue.get()
-        d.addCallback(self.sendLogMessage)
+        self.connected = True
+        self.queue = self.log_provider.attach()
+
+        self.queueDeferred = self.queue.get()
+        self.queueDeferred.addCallback(self.sendLogMessage)
 
     def sendLogMessage(self, msg):
         self.transport.write(json.dumps(msg))
+
+        # Repeat with the next message from the queue.
+        self.queueDeferred = self.queue.get()
+        self.queueDeferred.addCallback(self.sendLogMessage)
 
     def connectionLost(self, reason):
         self.factory.transports.remove(self.transport)
         out.info('sockjs /logs disconnected')
 
+        self.connected = False
+        self.log_provider.detach()
+
+        # If there is a Deferred waiting for the next message, cancel it.
+        if self.queueDeferred is not None:
+            self.queueDeferred.cancel()
+
 
 class LogSockJSFactory(Factory):
-    def __init__(self, log_provider):
-        self.log_provider = log_provider
+    def __init__(self, chutename):
+        self.chutename = chutename
         self.transports = set()
 
     def buildProtocol(self, addr):
-        return LogSockJSProtocol(self)
+        log_provider = LogProvider(self.chutename)
+        return LogSockJSProtocol(self, log_provider)
--- paradrop/src/paradrop/core/container/log_provider.py
+++ paradrop/src/paradrop/core/container/log_provider.py
@@ -10,15 +10,61 @@ from twisted.internet.defer import DeferredQueue
 class LogProvider(object):
     def __init__(self, chutename):
         self.chutename = chutename
+        self.queue = DeferredQueue()
+        self.listening = False
 
     def attach(self):
-        queue = DeferredQueue()
-        reactor.callInThread(self.__follow(queue))
-        return queue
+        """
+        Start listening for log messages.
 
-    def __follow(self, queue):
+        Returns a DeferredQueue that can be used by an async consumer.
+
+        Log messages in the queue will appear like the following:
+        {
+            'timestamp': '2017-01-30T15:46:23.009397536Z',
+            'message': 'Something happened'
+        }
+        """
+        reactor.callInThread(self.__follow)
+        self.listening = True
+        return self.queue
+
+    def detach(self):
+        """
+        Stop listening for log messages.
+
+        After this is called, no additional messages will be added to the
+        queue.
+        """
+        self.listening = False
+
+    def __follow(self):
+        """
+        Iterate over log messages from a container and add them to the queue
+        for consumption.  This function will block and wait for new messages
+        from the container.  Use the queue to interface with async code.
+        """
         client = docker.Client(base_url="unix://var/run/docker.sock", version='auto')
         output = client.logs(self.chutename, stdout=True, stderr=True,
                 stream=True, timestamps=True, follow=True)
         for line in output:
-            queue.put(line)
+            if not self.listening:
+                break
+
+            # I have grown to distrust Docker streaming functions.  It may
+            # return a string; it may return an object.  If it is a string,
+            # separate the timestamp portion from the rest of the message.
+            if isinstance(line, basestring):
+                parts = line.split(" ", 1)
+                if len(parts) > 1:
+                    self.queue.put({
+                        'timestamp': parts[0],
+                        'message': parts[1].rstrip()
+                    })
+
+                else:
+                    self.queue.put({
+                        'message': line.rstrip()
+                    })
+            else:
+                self.queue.put(line)
