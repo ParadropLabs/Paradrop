9f62e6188956d599ee97d3e2e6b0b4cbec630743
Peng Liu <pengliu.mail@gmail.com>
2017-02-21 15:45:25 -0600

    Fixed test case failures due to folder structure change

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,5 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('pdinstall'))
-sys.path.insert(0, os.path.abspath('paradrop/src'))
+sys.path.insert(0, os.path.abspath('paradrop/daemon'))
 sys.path.insert(0, os.path.abspath('tests/mocks'))
