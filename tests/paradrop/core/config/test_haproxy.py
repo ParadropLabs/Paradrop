a4aa5b63d5a0bddcef0fdf2d10340b7bc2a269d0
Lance Hartung <hartung@cs.wisc.edu>
2017-04-05 15:40:04 -0500

    Fix problems with unit tests that occur on CI.

--- tests/paradrop/core/config/test_haproxy.py
+++ tests/paradrop/core/config/test_haproxy.py
@@ -6,15 +6,23 @@ from nose.tools import assert_raises
 from paradrop.core.config import haproxy
 
 
+@patch("paradrop.core.config.haproxy.ChuteContainer")
 @patch("paradrop.core.config.haproxy.ChuteStorage.getChuteList")
-def test_generateConfigSections(getChuteList):
+def test_generateConfigSections(getChuteList, ChuteContainer):
     chute1 = MagicMock()
     chute1.name = "chute1"
 
     getChuteList.return_value = [chute1]
 
+    container = MagicMock()
+    container.isRunning.return_value = True
+    ChuteContainer.return_value = container
+
     sections = haproxy.generateConfigSections()
     assert len(sections) > 0
+
+    # Make sure one of the sections is for chute1.
+    assert any(chute1.name in v['header'] for v in sections)
 
 
 def test_writeConfigFile():
--- tests/paradrop/core/config/test_resource.py
+++ tests/paradrop/core/config/test_resource.py
@@ -10,8 +10,21 @@ def test_computeResourceAllocation():
     assert resource.computeResourceAllocation(chutes) == {}
 
     chutes = [MagicMock(), MagicMock()]
+
+    chutes[0].name = "chute1"
     chutes[0].isRunning.return_value = True
-    chutes[0].isRunning.return_value = False
+    chutes[0].resources = {}
+
+    chutes[1].name = "chute2"
+    chutes[1].isRunning.return_value = False
+    chutes[1].resources = {}
 
     allocation = resource.computeResourceAllocation(chutes)
     assert len(allocation) == 1
+
+    chutes[1].name = "chute2"
+    chutes[1].isRunning.return_value = True
+    chutes[1].resources = {}
+
+    allocation = resource.computeResourceAllocation(chutes)
+    assert len(allocation) == 2
