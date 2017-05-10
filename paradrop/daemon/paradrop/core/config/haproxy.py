035c6d5296529208fbf29a53fafde1a7ab97cff1
Lance Hartung <hartung@cs.wisc.edu>
2017-05-10 11:16:31 -0500

    haproxy redirect preserves original path and query string.

--- paradrop/daemon/paradrop/core/config/haproxy.py
+++ paradrop/daemon/paradrop/core/config/haproxy.py
@@ -37,11 +37,20 @@ def generateConfigSections():
         ]
     })
 
+    # Custom variables:
+    # - req.querymarker: will be set to the literal "?" if the original request
+    # contains a query string.  We will use this to construct a redirect with a
+    # query string only if needed.
+    # - req.subpath: will be set to the remainder of the path, if anything,
+    # after removing /chutes/<chutename>, e.g. "/chutes/hello-world/index.html"
+    # becomes "/index.html".  This does not include the query string.
     frontend = {
         "header": "frontend http-in",
         "lines": [
             "bind *:80",
-            "default_backend portal"
+            "default_backend portal",
+            "http-request set-var(req.querymarker) str(?) if { query -m found }",
+            "http-request set-var(req.subpath) path,regsub(^/chutes/[^/]+,)"
         ]
     }
     sections.append(frontend)
@@ -65,16 +74,18 @@ def generateConfigSections():
 
         # Generate rules that matches the beginning of the URL.
         # We need to be careful and either have an exact match
-        # or make sure there is a slash after the chute name to
-        # avoid mix-ups, e.g. "sticky-board" and "sticky-board-new".
+        # or make sure there is a slash or question mark after the chute name
+        # to avoid mix-ups, e.g. "sticky-board" and "sticky-board-new".
         frontend['lines'].append("acl path_{} url /chutes/{}".format(
             chute.name, chute.name))
         frontend['lines'].append("acl path_{} url_beg /chutes/{}/".format(
             chute.name, chute.name))
+        frontend['lines'].append("acl path_{} url_beg /chutes/{}?".format(
+            chute.name, chute.name))
 
         # Try to find a host binding for the web port to redirect:
-        # http://<host addr>/chutes/<chute> ->
-        # http://<host addr>:<chute port>
+        # http://<host addr>/chutes/<chute>/<path> ->
+        # http://<host addr>:<chute port>/<path>
         #
         # We need to do a lookup because the host port might be dynamically
         # assigned by Docker.
@@ -87,7 +98,7 @@ def generateConfigSections():
         if len(portconf) > 0:
             # TODO: Are there other elements in the list?
             binding = portconf[0]
-            frontend['lines'].append("http-request redirect location http://%[req.hdr(host)]:{} code 302 if path_{}".format(
+            frontend['lines'].append("http-request redirect location http://%[req.hdr(host)]:{}%[var(req.subpath)]%[var(req.querymarker)]%[query] code 302 if path_{}".format(
                 binding['HostPort'], chute.name))
 
         # Add a server at the chute's IP address.
