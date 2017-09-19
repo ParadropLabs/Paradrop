b256f2733d32b55e6a3a7ebfa1300b1a13555bab
Lance Hartung <hartung@cs.wisc.edu>
2017-09-19 14:32:33 -0500

    Add GitPython dependency for pdtools.

--- tools/pdtools/setup.py
+++ tools/pdtools/setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'click>=6.7',
         'future>=0.16.0',
+        'GitPython>=2.1.5',
         'PyYAML>=3.12',
         'requests>=2.18.1',
         'six>=1.10.0'
