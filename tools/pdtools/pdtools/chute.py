82a429a03d33f4ce462978dd26664f5c4f475946
Lance Hartung <hartung@cs.wisc.edu>
2017-09-19 10:30:55 -0500

    Added create-wifi-interface command to pdtools.

--- tools/pdtools/pdtools/chute.py
+++ tools/pdtools/pdtools/chute.py
@@ -6,6 +6,59 @@ import yaml
 from .helpers.chute import build_chute
 
 
+def update_object(obj, path, callback=None):
+    """
+    Traverse a data structure ensuring all nodes exist.
+
+    obj: expected to be a dictionary
+    path: string with dot-separated path components
+    callback: optional callback function (described below)
+
+    When update_object reaches the parent of the leaf node, it calls the
+    optional callback function. The arguments to the callback function are:
+
+    - parent: dictionary containing the leaf node
+    - key: string key for the leaf node in parent
+    - created: boolean flag indicating whether any part of the path, including
+        the leaf node needed to be created.
+
+    If the callback function is None, update_object will still ensure that all
+    components along the path exist. If the leaf needs to be created, it will
+    be created as an empty dictionary.
+
+    Example:
+    update_object({}, 'foo.bar') -> {'foo': {'bar': {}}}
+
+    Return value: Returns either the return value of callback, or if callback
+    is None, returns the value of the leaf node.
+    """
+    parts = path.split(".")
+
+    current = obj
+    parent = obj
+    created = False
+    for part in parts:
+        if len(part) == 0:
+            raise Exception("Path ({}) is invalid".format(path))
+
+        if not isinstance(current, dict):
+            raise Exception("Cannot set {}, not a dictionary".format(path))
+
+        # Create dictionaries along the way if path nodes do not exist,
+        # but make note of the fact that the previous value did not exist.
+        if part not in current:
+            current[part] = {}
+            created = True
+
+        parent = current
+        current = parent[part]
+
+    if callback is not None:
+        return callback(parent, parts[-1], created)
+    else:
+        return current
+
+
 @click.group()
 @click.pass_context
 def chute(ctx):
@@ -13,6 +66,44 @@ def chute(ctx):
     Sub-tree for building chutes.
     """
     pass
+
+
+@chute.command('create-wifi-interface')
+@click.pass_context
+@click.argument('essid')
+def create_wifi_interface(ctx, essid):
+    """
+    Add a Wi-Fi interface (AP mode) to the chute configuration.
+    """
+    with open('paradrop.yaml', 'r') as source:
+        chute = yaml.safe_load(source)
+
+    # Make sure the config.net section exists.
+    net = update_object(chute, 'config.net')
+
+    # Only support adding the first Wi-Fi interface for now.
+    if 'wifi' in net:
+        print("Wi-Fi interface already exists in paradrop.yaml.")
+        print("Please edit the file directly to make changes.")
+        return
+
+    net['wifi'] = {
+        'type': 'wifi',
+        'intfName': 'wlan0',
+        'dhcp': {
+            'lease': '12h',
+            'start': 4,
+            'limit': 250
+        },
+        'ssid': essid,
+        'options': {
+            'isolate': False,
+            'hidden': False
+        }
+    }
+
+    with open('paradrop.yaml', 'w') as output:
+        yaml.safe_dump(chute, output, default_flow_style=False)
 
 
 @chute.command()
@@ -42,9 +133,9 @@ def init(ctx):
 
 @chute.command()
 @click.pass_context
-@click.argument('key')
+@click.argument('path')
 @click.argument('value')
-def set(ctx, key, value):
+def set(ctx, path, value):
     """
     Set a value in the chute configuration file.
 
@@ -53,36 +144,21 @@ def set(ctx, key, value):
     with open('paradrop.yaml', 'r') as source:
         chute = yaml.safe_load(source)
 
-    parts = key.split(".")
-
     # Calling yaml.safe_load on the value does a little type interpretation
     # (e.g. numeric vs. string) and allows the user to directly set lists and
     # other structures.
     value = yaml.safe_load(value)
 
-    parent = chute
-    current = chute
-    created = False
-    for part in parts:
-        if len(part) == 0:
-            raise Exception("Key ({}) is invalid".format(key))
+    def set_value(parent, key, created):
+        if created:
+            print("Creating new field {} = {}".format(path, value))
+        else:
+            current = parent[key]
+            print("Changing {} from {} to {}".format(path, current, value))
 
-        parent = current
+        parent[key] = value
 
-        # Create dictionaries along the way if path nodes do not exist,
-        # but make note of the fact that the previous value did not exist.
-        if part not in parent:
-            parent[part] = {}
-            created = True
-
-        current = parent[part]
-
-    if created:
-        print("Creating new field {} = {}".format(key, value))
-    else:
-        print("Changing {} from {} to {}".format(key, current, value))
-
-    parent[parts[-1]] = value
+    update_object(chute, path, set_value)
 
     with open('paradrop.yaml', 'w') as output:
         yaml.safe_dump(chute, output, default_flow_style=False)
