5918fa686e916b8500806bd5be6a55ddc87a81ea
Lance Hartung <hartung@cs.wisc.edu>
2017-10-11 11:25:02 -0500

    Add test cases for fulfillDeviceRequest.

--- tests/paradrop/core/config/test_network.py
+++ tests/paradrop/core/config/test_network.py
@@ -1,7 +1,9 @@
+import collections
 from nose.tools import assert_raises
 from mock import MagicMock, Mock, patch
 
 from paradrop.core.config import network
+from paradrop.core.config.reservations import DeviceReservations
 from paradrop.core.update.update_object import UpdateObject
 
 
@@ -69,3 +71,98 @@ def test_getNetworkConfig():
     # Should have passed through the dhcp section.
     iface = value[0]
     assert 'dhcp' in iface
+
+
+def test_fulfillDeviceRequest():
+    update = MagicMock()
+
+    reservations = collections.defaultdict(DeviceReservations)
+    update.new.getCache.return_value = reservations
+
+    devices = {
+        'wifi': [
+            {'name': 'wlan0'},
+            {'name': 'wlan1'}
+        ],
+        'lan': [
+            {'name': 'eth0'},
+            {'name': 'eth1'}
+        ]
+    }
+
+    # Test exclusive access type interfaces (monitor).
+    #
+    # The first interface is assigned to the first device, second interface
+    # to the second device, and the third raises an exception.
+
+    config = {
+        'type': 'wifi',
+        'mode': 'monitor'
+    }
+
+    result = network.fulfillDeviceRequest(update, config, devices)
+    assert result['name'] == 'wlan0'
+    result = network.fulfillDeviceRequest(update, config, devices)
+    assert result['name'] == 'wlan1'
+    assert_raises(Exception, network.fulfillDeviceRequest, update, config, devices)
+
+    reservations = collections.defaultdict(DeviceReservations)
+    update.new.getCache.return_value = reservations
+
+    # Test shared access type interfaces (AP).
+    #
+    # This should assign several consecutive interfaces to the first device,
+    # then to the second, then finally raise an exception.
+
+    config = {
+        'type': 'wifi',
+        'mode': 'ap'
+    }
+
+    for i in range(network.MAX_AP_INTERFACES):
+        result = network.fulfillDeviceRequest(update, config, devices)
+        print(result)
+        assert result['name'] == 'wlan0'
+    for i in range(network.MAX_AP_INTERFACES):
+        result = network.fulfillDeviceRequest(update, config, devices)
+        assert result['name'] == 'wlan1'
+    assert_raises(Exception, network.fulfillDeviceRequest, update, config, devices)
+
+    reservations = collections.defaultdict(DeviceReservations)
+    update.new.getCache.return_value = reservations
+
+    # Test mixed assignment.
+    #
+    # The first interface gains exclusive access to the first device.
+    # Following interfaces gain shared access to the second device.
+
+    config = {
+        'type': 'wifi',
+        'mode': 'monitor'
+    }
+
+    result = network.fulfillDeviceRequest(update, config, devices)
+    assert result['name'] == 'wlan0'
+
+    config = {
+        'type': 'wifi',
+        'mode': 'ap'
+    }
+
+    for i in range(network.MAX_AP_INTERFACES):
+        result = network.fulfillDeviceRequest(update, config, devices)
+        print(result)
+        assert result['name'] == 'wlan1'
+    assert_raises(Exception, network.fulfillDeviceRequest, update, config, devices)
+
+    # Test lan type interfaces.
+
+    config = {
+        'type': 'lan'
+    }
+
+    result = network.fulfillDeviceRequest(update, config, devices)
+    assert result['name'] == 'eth0'
+    result = network.fulfillDeviceRequest(update, config, devices)
+    assert result['name'] == 'eth1'
+    assert_raises(Exception, network.fulfillDeviceRequest, update, config, devices)
