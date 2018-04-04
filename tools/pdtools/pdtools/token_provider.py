7cd85fe85a0681fdba1e157c55a900a1dcc7e2cb
Lance Hartung <hartung@cs.wisc.edu>
2018-04-04 10:13:47 -0500

    Added documentation to token provider module.

--- tools/pdtools/pdtools/token_provider.py
+++ tools/pdtools/pdtools/token_provider.py
@@ -1,3 +1,42 @@
+"""
+The TokenProvider abstraction allows for different methods of acquiring a token
+for authorization with an HTTP service. As shown in the table below, multiple
+methods of acquiring a token are generally available, but their applicability
+depends on whether the code is running interactively or inside a chute where
+there is no user input and whether the target API is the node or the cloud
+controller.  This creates a fair bit of complexity for the AuthenticatedClient
+to perform an authenticated API call.
+
+Method               | Caller    | Target API
+(x = applicable)     | CLI Chute | Node Cloud
+---------------------------------------------
+Environment variable | x   x     | x    -
+Saved locally        | x   -     | x    x
+Default password     | x   -     | x    -
+Prompt for input     | x   -     | x    x
+
+Here is an example flow for the more complicated case, interacting with a
+node's API:
+
+1. We check for an environment variable because chutes authenticate using
+   a token provided through the PARADROP_API_TOKEN variable.
+2. We check for a token saved to a file. We save tokens locally so that
+   users do not need to type their password with each command.
+3. If there is not a saved token or if the saved token has expired, then
+   we can try a default password. Most nodes intended for development
+   or education purposes retain their default settings.
+4. Finally, if user input is available, we can prompt the user to enter
+   a username and password. We should generally never prompt for a
+   password in code that runs inside a chute. That code is presumed to
+   be non-interactive, and it would be better just to return an error.
+
+The TokenProvider abstraction helps simplify the logic above.  It allows an
+AuthenticatedClient implementation to instantiate one or more parameterized
+TokenProvider objects. With a list of provider objects, the sequence of steps
+above amounts to iterating the list and calling `get_token` on each provider
+until one of them succeeds.
+"""
+
 import getpass
 import os
 import sys
