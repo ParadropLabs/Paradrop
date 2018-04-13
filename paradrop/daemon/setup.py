6ec66b5dbff45612bac1a9ad43797711f59cfdb9
Lance Hartung <hartung@cs.wisc.edu>
2018-04-13 14:51:30 -0500

    Release version 0.11.0

--- paradrop/daemon/setup.py
+++ paradrop/daemon/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="paradrop",
-    version='0.10.3',
+    version='0.11.0',
     author="ParaDrop Labs",
     description="ParaDrop wireless virtualization",
     license="GPL",
