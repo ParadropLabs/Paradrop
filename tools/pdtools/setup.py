b62b4df6ffe98b3e27236d02506d8da7035ae125
Lance Hartung <hartung@cs.wisc.edu>
2019-01-17 13:17:59 -0600

    Release version 0.13.0

--- tools/pdtools/setup.py
+++ tools/pdtools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdtools",
-    version='0.12.7',
+    version='0.13.0',
     author="ParaDrop Labs",
     description="ParaDrop development tools",
     install_requires=[
