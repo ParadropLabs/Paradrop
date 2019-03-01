2d2c10611c0c16df66eca616cb2092ddb1e2c224
Lance Hartung <hartung@cs.wisc.edu>
2019-03-01 10:37:13 -0600

    Add more project information to the pdtools setup.py.

--- tools/pdtools/setup.py
+++ tools/pdtools/setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("../../README.md", "r") as source:
+    long_description = source.read()
+
 setup(
     name="pdtools",
     version='0.13.1',
-    author="ParaDrop Labs",
-    description="ParaDrop development tools",
+    description="Paradrop Developer Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://paradrop.org",
+
+    project_urls = {
+        "Documentation": "https://paradrop.readthedocs.io/en/latest/",
+        "Homepage": "https://paradrop.org",
+        "Source": "https://github.com/ParadropLabs/Paradrop",
+    },
+
+    packages=find_packages(),
+
     install_requires=[
         'appdirs~=1.4.3',
         'arrow~=0.10.0',
@@ -18,7 +32,25 @@ setup(
         'websocket-client~=0.40.0'
     ],
 
-    packages=find_packages(),
+    python_requires=">=2.7",
+
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+
+    maintainer="Lance Hartung",
+    maintainer_email="lance@paradrop.io",
+
     package_dir={'pdtools': 'pdtools'},
     package_data={'pdtools': ['schemas/*.json']},
     include_package_data=True,
